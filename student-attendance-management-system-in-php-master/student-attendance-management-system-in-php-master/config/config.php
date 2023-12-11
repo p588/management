1a88292ac42254b662e37752abebb49ac4c9a054
MATAM PREETHI <74255142+p588@users.noreply.github.com>
2023-12-12 00:18:32 +0530

    Update config.php

--- student-attendance-management-system-in-php-master/student-attendance-management-system-in-php-master/config/config.php
+++ student-attendance-management-system-in-php-master/student-attendance-management-system-in-php-master/config/config.php
@@ -1,6 +1,6 @@
     <?php
-	    define("DB_HOST", "localhost");
-	    define("DB_USER", "root");
-	    define("DB_PASS", "");
+	    define("DB_HOST", "preethi0987.mysql.database.azure.com");
+	    define("DB_USER", "preethi1234");
+	    define("DB_PASS", "Padmaja@12345");
 	    define("DB_NAME", "db_sams");
     ?>
